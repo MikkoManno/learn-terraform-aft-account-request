06caf63844ce9d131006a17828979a2471b99207
Mikko Mannonen <mikko.mannonen@G0693.local>
2022-11-01 09:13:24 +0200

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "aws-gofore-cloud-management+sandbox-aft@gofore.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "aws-gofore-cloud-management+sandbox-aft@gofore.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
