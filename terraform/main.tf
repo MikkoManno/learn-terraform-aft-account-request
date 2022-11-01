8fd44c686e611769ab37d6eaf4fddb4d8f1dda2a
Mikko Mannonen <mikko.mannonen@G0693.local>
2022-11-01 11:32:57 +0200

    Trying add 3th account

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "aws-gofore-cloud-management+sandbox-aft@gofore.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "dev"
+    ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "aws-gofore-cloud-management+sandbox-aft@gofore.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
@@ -31,7 +31,7 @@ module "sandbox2" {
 
   control_tower_parameters = {
     AccountEmail              = "aws-gofore-cloud-management+sandbox-aft2@gofore.com"
-    AccountName               = "sandbox-aft"
+    AccountName               = "sandbox-aft2"
     ManagedOrganizationalUnit = "dev"
     SSOUserEmail              = "aws-gofore-cloud-management+sandbox-aft2@gofore.com"
     SSOUserFirstName          = "Sandbox2"
@@ -54,3 +54,30 @@ module "sandbox2" {
   account_customizations_name = "sandbox2"
 }
 
+module "sadaft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-gofore-cloud-management+sadaft@gofore.com"
+    AccountName               = "sandbox-sadaft"
+    ManagedOrganizationalUnit = "dev"
+    SSOUserEmail              = "aws-gofore-cloud-management+sadaft@gofore.com"
+    SSOUserFirstName          = "Sadaft"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sadaft"
+}
