95f1c1bc8fb3a466e2135977029f14d805d6eca3
Mikko Mannonen <mikko.mannonen@G0693.local>
2022-11-02 09:10:22 +0200

    Again one more time accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,3 +81,31 @@ module "sadaft" {
 
   account_customizations_name = "sadaft"
 }
+
+module "happyaft" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-gofore-cloud-management+happyaft@gofore.com"
+    AccountName               = "sandbox-happyaft"
+    ManagedOrganizationalUnit = "dev"
+    SSOUserEmail              = "aws-gofore-cloud-management+happyaft@gofore.com"
+    SSOUserFirstName          = "Happyaft"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "happyaft"
+}
