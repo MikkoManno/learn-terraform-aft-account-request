c5948645529a94797ed5d49c0f1d2200b7846762
Mikko Mannonen <mikko.mannonen@G0693.local>
2022-11-07 11:05:52 +0200

    Adding urlmonster account

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,3 +109,31 @@ module "happyaft" {
 
   account_customizations_name = "happyaft"
 }
+
+module "urlmonster" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "aws-gofore-cloud-management+urlmonster@gofore.com"
+    AccountName               = "urlmonster"
+    ManagedOrganizationalUnit = "dev"
+    SSOUserEmail              = "aws-gofore-cloud-management+urlmonster@gofore.com"
+    SSOUserFirstName          = "URL"
+    SSOUserLastName           = "Monster"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "MM"
+    change_reason       = "Testi tili short urlia varten"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "urlmonster"
+}
